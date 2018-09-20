a88d82c228e0134d7b1646b81fa79c324b6155d9
huqi <huqi@tangbull.com>
2018-09-20 19:30:08 +0800

    init

---
+++ app01/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+
+import time,datetime
+def show_time(request):
+    t=datetime.datetime.now()
+    # return HttpResponse("<html><body>It is now %s </body></html>" %t)
+
+    return render(request,"show_time.html",{"time":t})
+
+
+
+
+class Animal():
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+def query(request):
+
+    l=["zhang","村长","书记"]
+    d={"name":"校长","age":12}
+    c=Animal("alex",'公')
+
+    test="hello world"
+    num=100
+
+    t=datetime.datetime.now()
+    list=[]
+
+    a="<a href='http://www.baidu.com' target='_blank'>click</a>"
+
+    return render(request,"index.html",locals())
